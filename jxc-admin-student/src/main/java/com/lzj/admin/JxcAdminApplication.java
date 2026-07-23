ae2220a5094d97d1ac00ea160aec3710452da2ce
SETSU486925 <1803275663@qq.com>
2026-07-23 17:27:28 +0800

    day1

---
+++ jxc-admin-student/src/main/java/com/lzj/admin/JxcAdminApplication.java
@@ -0,0 +1,27 @@
+package com.lzj.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 进销存系统启动器
+ * @author xmc
+ * @date 2026/7/23
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+@MapperScan(basePackages = "com.lzj.admin.mapper")
+public class JxcAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxcAdminApplication.class, args);
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ jxc-admin-student/src/main/java/com/lzj/admin/query/CustomerReturnListGoodsQuery.java
@@ -0,0 +1,29 @@
+package com.lzj.admin.query;
+
+import lombok.Data;
+
+/**
+ * 乐字节  踏实教育 用心服务
+ *
+ * @author 乐字节--老李
+ * @version 1.0
+ */
+@Data
+public class CustomerReturnListGoodsQuery extends BaseQuery{
+    private Integer customerReturnListId;
+
+	public long getLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getCustomerReturnListId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public long getPage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
